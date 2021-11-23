d1b1a24917111cc65807707c94742e9e0cf9bfe7
Pan Luo <pan.luo@ubc.ca>
2021-11-23 15:03:44 -0800

    Make DB configurable

--- settings/prod.py
+++ settings/prod.py
@@ -1,1 +1,17 @@
+import json
+
 from .base import *
+
+DEFAULT_DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+DATABASES = (
+  json.loads(os.environ["APP_DATABASES"])
+  if "APP_DATABASES" in os.environ
+  else DEFAULT_DATABASES
+)
